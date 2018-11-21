aa48bcd2b1ed570dc2ccd0ea6e87f8968bffec2e
djklim87 <djklim87@gmail.com>
2018-11-21 18:31:52 +0200

    8

--- sender.php
+++ sender.php
@@ -2,15 +2,17 @@
 
 require_once 'Core/Logger.php';
 
-if(empty($_POST['message'])){
+Logger::log('Sender script: New message');
+
+if (empty($_POST['message'])) {
 
     echo "Error: Sending message can't be empty!\n";
 
-}elseif (empty($_POST['topic'])){
+} elseif (empty($_POST['topic'])) {
 
     echo "Error: Topic for sending can't be empty!\n";
 
-}else{
+} else {
 
     json_decode($_POST['message']);
     if (json_last_error() === JSON_ERROR_NONE) {
@@ -23,7 +25,7 @@ if(empty($_POST['message'])){
         $producer = new KafkaProducer($config);
 
         $producer->send($_POST['topic'], $_POST['message']);
-
+        Logger::log('Sender script: Message ' . $_POST['message'] . ' sended to Kafka topic ' . $_POST['topic']);
     } else {
 
         echo "Error: Incompatible data format!\n";
